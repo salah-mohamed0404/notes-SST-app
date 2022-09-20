2b5356cf2b7ed6fd47adaa14a04004cfd49458b5
salahcst344 <salahcst344@gmail.com>
2022-09-20 18:40:31 +0200

    Create the API

---
+++ stacks/ApiStack.js
@@ -0,0 +1,31 @@
+import { Api, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+    },
+    routes: {
+      "POST /notes": "functions/create.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
