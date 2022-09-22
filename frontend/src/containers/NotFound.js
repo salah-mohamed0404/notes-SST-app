19c61ad5d2693a9beab705c289a38fbe4e5b72de
salahcst344 <salahcst344@gmail.com>
2022-09-22 16:34:43 +0200

    Add react routes

---
+++ frontend/src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound text-center">
+      <h3>😭☹Sorry, page not found!☹😭</h3>
+    </div>
+  );
+}
