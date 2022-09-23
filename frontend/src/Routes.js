46082792fea6bf24a54190ecb7bfbd1c125ee40b
salahcst344 <salahcst344@gmail.com>
2022-09-23 14:20:50 +0200

    Add signup

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
 export default function Links() {
@@ -9,6 +10,7 @@ export default function Links() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
